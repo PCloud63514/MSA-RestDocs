891aa93f602cff8f14b1a4681d5c6d9e1d51b12f
PCloud63514 <pcloud63514@gmail.com>
2022-01-16 15:59:27 +0900

    update

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 여러 패턴과 아키텍쳐를 학습하기 위한 프로젝트
 
 keyword
+
 - MSA
 - Eventual Consistency
 - Message Queue
@@ -10,6 +11,5 @@ keyword
 - Clean Architecture
 - UI Composite
 - Micro Frontend
-
 
 <img width="576" alt="스크린샷 2021-12-23 오후 10 41 53" src="https://user-images.githubusercontent.com/22608825/147248764-f1503117-02f6-497b-bac3-00616b2ccabf.png">
---
+++ src/main/java/org/pcloud/hexagonalserver/HexagonalServerApplication.java
@@ -0,0 +1,13 @@
+package org.pcloud.hexagonalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalServerApplication.class, args);
+    }
+
+}
