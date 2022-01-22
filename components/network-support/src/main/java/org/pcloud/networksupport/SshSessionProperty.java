5ecc85435899cf3ce8054872de620a759a0195df
PCloud63514 <pcloud63514@gmail.com>
2022-01-22 23:19:56 +0900

    Network Support 패키지 - JSch 라이브러리 추가 - SshConnection 작성 완료 - SshSessionProperty 내용 추가

--- components/network-support/src/main/java/org/pcloud/networksupport/SshSessionProperty.java
+++ components/network-support/src/main/java/org/pcloud/networksupport/SshSessionProperty.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 @Getter
 public class SshSessionProperty {
     /**
-     * Session의 이름입니다.
+     * Session의 이름입니다. 특정 세션을 조회, 종료 등에 쓰입니다. 필수입니다.
      */
     private final String sessionName;
 
+    /**
+     * ssh 연결에 대상 이름입니다.
+     */
     private final String userName;
     /**
      * host ip address required for Ssh connection.
@@ -19,9 +22,19 @@ public class SshSessionProperty {
      */
     private final int port;
 
+    /**
+     * ssh 연결에 필요한 패스워드입니다.
+     */
     private final String password;
 
+    /**
+     * ssh 연결에 사용할 임의의 사용자의 연결 포트입니다. 중복을 피해야합니다.
+     *
+     */
     private final int lPort;
+    /**
+     * ssh 연결에 사용할 대상의 포트입니다.
+     */
     private final int rPort;
 
     public SshSessionProperty(String sessionName, String userName, String host, int port, int lPort, int rPort) {
