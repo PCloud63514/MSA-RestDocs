370e21f9aa56781812af606a7dd7fcd6adab7516
PCloud <pcloud63514@gmail.com>
2022-01-28 10:21:47 +0900

    불필요 항목 제거

---
+++ applications/app-event/src/main/java/org/pcloud/appevent/AppEventApplication.java
@@ -0,0 +1,12 @@
+package org.pcloud.appevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AppEventApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppEventApplication.class, args);
+    }
+}
--- components/administrator/src/main/java/org/pcloud/administrator/AdministratorService.java
+++
@@ -1,4 +0,0 @@
-package org.pcloud.administrator;
-
-public interface AdministratorService {
-}
